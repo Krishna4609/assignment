6c17ffd6fd5849ee91e3d5ea5955271b782cf9fe
Vaishnavi-Hegde17 <vaishnavi.scs24@bmsce.ac.in>
2025-04-23 20:43:44 +0530

    small change

--- Linkedlist/linkedlist.py
+++ Linkedlist/linkedlist.py
@@ -32,7 +32,7 @@ class LinkedList:
             prev = current
             current = current.next
 
-        print(f"{data} not found in the list.")
+        print(f"{data} not found in the list1.")
 
     def display(self):
         current = self.head
